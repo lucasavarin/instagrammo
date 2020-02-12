027286268e607c85192045694827c9f26c4712b3
muusta45 <muusta45@gmail.com>
2020-02-12 11:36:59 +0100

    First Commit

---
+++ app/src/main/java/com/mst/instagrammo/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.mst.instagrammo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
