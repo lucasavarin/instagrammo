04c655f248103b0340d154705672c872536dfc19
luca <lscutillo@gmail.com>
2020-02-12 17:39:57 +0100

    gestione login e aggiunta libreria gradle

--- app/src/main/java/com/example/login/MainActivity.kt
+++ app/src/main/java/com/example/login/MainActivity.kt
@@ -3,12 +3,10 @@ package com.example.login
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import androidx.core.app.ComponentActivity.ExtraData
-import androidx.core.content.ContextCompat.getSystemService
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
-import android.view.View
-import android.widget.Button
 import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
 class MainActivity : AppCompatActivity() {
@@ -18,13 +16,22 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         login.setOnClickListener{
-            val user = user.text.toString()
+            val username = user.text.toString()
             val password = password.text.toString()
 
-//            Toast.makeText(, "Logged in", Toast.LENGTH_LONG).show()
+            ApiClient.getUser.getUser(AuthRequest(username,password)).enqueue(object : Callback<AuthResponse>{
+                override fun onFailure(call: Call<AuthResponse>, t: Throwable) {
+                    Toast.makeText(this@MainActivity, "hai sbagliato credenziali", Toast.LENGTH_LONG).show()
+                }
+
+                override fun onResponse(
+                    call: Call<AuthResponse>,
+                    response: Response<AuthResponse>) {
+                    Toast.makeText(applicationContext, response.body().toString(), Toast.LENGTH_LONG)
+                }
+            })
+
         }
 
     }
-
-
 }
