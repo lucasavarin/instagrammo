f4b909a519183df50431c9ff4e6409f96101ece9
Scott Thushyanthan <scott.thushyanthan@lynxspa.com>
2020-02-12 11:19:56 +0100

    First Commit
    Initial commit

---
+++ app/src/main/java/thushyanthan/scott/javalynx/instagrammo/LoginActivity.kt
@@ -0,0 +1,15 @@
+package thushyanthan.scott.javalynx.instagrammo
+
+import android.app.Activity
+import android.os.Bundle
+import android.widget.TextView
+
+class LoginActivity: Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        //val tx = findViewById<TextView>(R.id.ciao)
+        //tx.setText("ehilaaaaaaa")
+    }
+}
