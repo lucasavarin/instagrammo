7589825a40039307797706e927917db6caa10bff
Andrea <andreborgo1998@gmail.com>
2020-02-12 15:40:00 +0100

    modify main.xml, logo e MainActivity

--- app/src/main/java/com/example/login/MainActivity.kt
+++ app/src/main/java/com/example/login/MainActivity.kt
@@ -2,11 +2,29 @@ package com.example.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        login.setOnClickListener{
+            val user = user.text.toString()
+            val password = password.text.toString()
+
+//            Toast.makeText(, "Logged in", Toast.LENGTH_LONG).show()
+        }
+
     }
+
+
 }
