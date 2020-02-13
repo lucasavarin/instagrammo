52d818b6d8e8d88298734fdb4d65744140a853b7
Kevin <kevin.bra99@gmail.com>
2020-02-13 10:25:41 +0100

    metodo do login, splash screen, icon

--- app/src/main/java/com/example/instagrammo/LoginActivity.kt
+++ app/src/main/java/com/example/instagrammo/LoginActivity.kt
@@ -3,22 +3,38 @@ package com.example.instagrammo
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.instagrammo.beans.request.AuthRequest
+import com.example.instagrammo.beans.response.AuthResponse
+import com.example.instagrammo.retrofit.Client
 import kotlinx.android.synthetic.main.activity_login.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class LoginActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        btn_submit.setOnClickListener{
-            val email = email_label.text
-            val pwd = label_pwd.text
+        btn_submit.setOnClickListener{ view-> doLogin()}
+    }
 
-            if(!email!!.contains("@")){
-                Toast.makeText(this, "Email errata", Toast.LENGTH_SHORT).show()
-            }else{
-                Toast.makeText(this, email, Toast.LENGTH_SHORT).show()
+    fun doLogin(){
+        val call = Client.getClient.doAuth(AuthRequest(email_label.text.toString(), label_pwd.text.toString()))
+
+        call.enqueue(object: Callback<AuthResponse>{
+            override fun onFailure(call: Call<AuthResponse>, t: Throwable) {
+                Toast.makeText(applicationContext,"'error",Toast.LENGTH_LONG).show()
             }
-        }
+
+            override fun onResponse(call: Call<AuthResponse>, response: Response<AuthResponse>) {
+                if (response.isSuccessful){
+                    Toast.makeText(applicationContext,"Successo",Toast.LENGTH_LONG).show()
+                }else{
+                    Toast.makeText(applicationContext,"Fallimento",Toast.LENGTH_LONG).show()
+                }
+            }
+
+        })
     }
 }
---
+++ app/src/main/java/com/example/instagrammo/SplashScreenActivity.kt
@@ -0,0 +1,23 @@
+package com.example.instagrammo
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashScreenActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Handler().postDelayed({
+            intent = Intent(applicationContext, LoginActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(R.anim.slide_up, R.anim.slide_down)
+        }, 3000)
+
+    }
+}
