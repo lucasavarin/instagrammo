4672df6fc2dfb91501943cc865dced79aca92e86
Ludovico <ludovico.lalli@gmail.com>
2020-03-13 14:32:08 +0100

    Partial Profile Developed

---
+++ app/src/main/java/thushyanthan/scott/javalynx/instagrammo/fragments/ProfileAdapter.kt
@@ -0,0 +1,8 @@
+package thushyanthan.scott.javalynx.instagrammo.fragments
+
+import android.content.Context
+
+class ProfileAdapter (val imgProfileAdapter: ProfileAdapter, val context: Context){
+    var imageUrl: String? = null
+   // var imgProfile: Observable<Drawble>? = null
+}
--- app/src/main/java/thushyanthan/scott/javalynx/instagrammo/fragments/ProfileFragment.kt
+++ app/src/main/java/thushyanthan/scott/javalynx/instagrammo/fragments/ProfileFragment.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.squareup.picasso.Picasso
 import thushyanthan.scott.javalynx.instagrammo.R
+
 
 class ProfileFragment: Fragment() {
     override fun onCreateView(
@@ -17,4 +19,5 @@ class ProfileFragment: Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
     }
+
 }
