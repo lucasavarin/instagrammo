abff28e09352a8c5fb1191192990071d015e319c
Sottob <matteobottos98@gmail.com>
2020-03-13 09:47:33 +0100

    HomePayloadPostBean.kt HomeProfilePostBean.kt HomeWrapperPostBean.kt

---
+++ app/src/main/java/com/example/instagrammo/beans/response/HomeWrapperPostBean.kt
@@ -0,0 +1,12 @@
+package com.example.instagrammo.beans.response
+
+import android.service.voice.AlwaysOnHotwordDetector
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+data class HomeWrapperPostBean(
+    @Expose()
+    @SerializedName(value = "result") val result : Boolean,
+    @Expose()
+    @SerializedName(value = "payload") val payload: List<HomePayloadPostBean>
+)
