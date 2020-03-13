9353e4c45c23131318ab27874aa529590a3d18c4
B0rgo <andreborgo1998@gmail.com>
2020-03-13 15:08:38 +0100

    add profile + modify_profile layout

--- app/src/main/java/com/example/view/profile_fragment/ProfileFragment.kt
+++ app/src/main/java/com/example/view/profile_fragment/ProfileFragment.kt
@@ -4,18 +4,47 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.annotation.ContentView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.example.View.modifyUserFragment.ModifyUserFragment
+import com.example.bean.buissnes.HomeWrapperResponse
+import com.example.bean.rest.response.AuthResponse
 import com.example.login.R
+import com.example.util.retrofit.ClientInterceptor
+import com.example.util.retrofit.Session
+import kotlinx.android.synthetic.main.profile_layout.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
 class ProfileFragment : Fragment() {
 
     override fun onCreateView(
+
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.profile_layout,container,false)
+        return inflater.inflate(R.layout.profile_layout,container ,false)
+
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        modifyButton.setOnClickListener{
+
+            val fragment = ModifyUserFragment()
+            val fragmentManager = activity!!.supportFragmentManager
+            val fragmentTransaction = fragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.fragment_container, fragment)
+            fragmentTransaction.addToBackStack(null)
+            fragmentTransaction.commit()
+        }
+    }
+
+
 
 }
