5222e95afbe581dffd826d4020357a2a2e0e4294
Scott Thushyanthan <scott.thushyanthan@lynxspa.com>
2020-03-16 18:56:34 +0100

    UI FIX Follower List and Posts List

--- app/src/main/java/thushyanthan/scott/javalynx/instagrammo/adapter/PostsAdapter.kt
+++ app/src/main/java/thushyanthan/scott/javalynx/instagrammo/adapter/PostsAdapter.kt
@@ -6,9 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.fragment_edit_profile.view.*
 import kotlinx.android.synthetic.main.post_list_item.view.*
 import thushyanthan.scott.javalynx.instagrammo.util.rest.PostPayload
 import thushyanthan.scott.javalynx.instagrammo.R
+import thushyanthan.scott.javalynx.instagrammo.util.transformation.CircleTransformation
 
 class HomeAdapter (val posts:List<PostPayload>, val context:Context) : RecyclerView.Adapter<ViewHolder>() {
 
@@ -38,7 +40,11 @@ class ViewHolder (view: View) : RecyclerView.ViewHolder(view) {
 
     fun assemblePost(post: PostPayload){
         this.post = post
-        Picasso.get().load(post.picture).into(v.imagePost)
+        if (post.profile.profilePicture!=""){
+            Picasso.get().load(post.profile.profilePicture).transform(CircleTransformation()).into(v.profPicture)
+        }
+
+        Picasso.get().load(post.picture).resize(1000, 700).into(v.imagePost)
         v.titlePost.text = post.title
         v.datePost.text = post.uploadTime
         v.username.text = post.profile.name
