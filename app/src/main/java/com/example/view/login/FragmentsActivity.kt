8263dc58e439d209a9fd5c350ac4ac6c700febaa
lscutillo <lscutillo@gmail.com>
2020-03-17 21:41:59 +0100

    sistemate icone bottomNavigationView

--- app/src/main/java/com/example/view/login/FragmentsActivity.kt
+++ app/src/main/java/com/example/view/login/FragmentsActivity.kt
@@ -5,8 +5,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import com.example.login.R
+import com.example.view.add_fragment.AddFragment
+import com.example.view.favourite_fragment.FavouriteFragment
 import com.example.view.home_fragment.HomeFragment
 import com.example.view.profile_fragment.ProfileFragment
+import com.example.view.search_fragment.SearchFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class FragmentsActivity : AppCompatActivity(){
@@ -31,6 +34,22 @@ class FragmentsActivity : AppCompatActivity(){
                 R.id.Profile -> {
                     loadFragment(ProfileFragment())
                     return@setOnNavigationItemSelectedListener true
+                }
+
+                R.id.Add -> {
+                    loadFragment(AddFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+
+                R.id.favourite -> {
+                    loadFragment(FavouriteFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+
+                R.id.search -> {
+                    loadFragment(SearchFragment())
+                    return@setOnNavigationItemSelectedListener true
+
                 }
 
                 else  -> return@setOnNavigationItemSelectedListener false
