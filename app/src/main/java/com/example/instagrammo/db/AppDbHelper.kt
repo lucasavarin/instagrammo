11b51be89c6e56e6c9ab1f455a97768d7ef8b0c4
Alessandro <alejolly98@gmail.com>
2020-04-01 15:57:48 +0200

    Add FollowerDB

--- app/src/main/java/com/example/instagrammo/db/AppDbHelper.kt
+++ app/src/main/java/com/example/instagrammo/db/AppDbHelper.kt
@@ -8,25 +8,24 @@ import android.provider.BaseColumns
 import android.util.Log
 import com.example.instagrammo.dbcontractclass.FollowerContract
 import com.example.instagrammo.dbcontractclass.PostContract
-import com.example.instagrammo.model.FollowerDB
-import com.example.instagrammo.model.Post
-import com.example.instagrammo.model.PostDb
-import com.example.instagrammo.model.Profile
+import com.example.instagrammo.model.*
 
 class AppDbHelper(ctx: Context) : SQLiteOpenHelper(ctx, DATABASE_NAME, null, DATABASE_VERSION) {
     private val CREATE_POST_TABLE = " CREATE TABLE ${PostContract.PostEntry.TABLE_NAME} (" +
-            "${BaseColumns._ID} INTEGER PRIMARY KEY ," +
-            "${PostContract.PostEntry.COLUMN_TITLE} TEXT ," +
-            "${PostContract.PostEntry.COLUMN_PROFILE_ID} INTEGER ," +
-            "${PostContract.PostEntry.COLUMN_UPLOADTIME} TEXT " +
-            " )"
+            "${PostContract.PostEntry.COLUMN_ID} INTEGER PRIMARY KEY ," +
+            "${PostContract.PostEntry.COLUMN_TITLE} TEXT, " +
+            "${PostContract.PostEntry.COLUMN_PROFILE_ID} INTEGER, " +
+            "${PostContract.PostEntry.COLUMN_UPLOADTIME} TEXT, " +
+            " CONSTRAINT fk_profile" +
+            " FOREIGN KEY (${PostContract.PostEntry.COLUMN_PROFILE_ID})" +
+            " REFERENCES ${FollowerContract.FollowerEntry.TABLE_NAME} (${FollowerContract.FollowerEntry.COLUMN_ID}))"
 
-    private val CREATE_FOLLOWER_TABLE = "CREATE TABLE ${FollowerContract.FollowerEntry.TABLE_NAME} ("+
-            "${FollowerContract.FollowerEntry.COLUMN_ID} INTEGER PRIMARY KEY ,"+
-            "${FollowerContract.FollowerEntry.COLUMN_NAME} TEXT ,"+
-            "${FollowerContract.FollowerEntry.COLUMN_PICTURE} INTEGER ,"+
-            "${FollowerContract.FollowerEntry.COLUMN_DESCRIPTION} TEXT )"
-
+    private val CREATE_FOLLOWER_TABLE =
+        "CREATE TABLE ${FollowerContract.FollowerEntry.TABLE_NAME} (" +
+                "${FollowerContract.FollowerEntry.COLUMN_ID} INTEGER PRIMARY KEY ," +
+                "${FollowerContract.FollowerEntry.COLUMN_NAME} TEXT ," +
+                "${FollowerContract.FollowerEntry.COLUMN_PICTURE} INTEGER ," +
+                "${FollowerContract.FollowerEntry.COLUMN_DESCRIPTION} TEXT )"
 
 
     override fun onCreate(db: SQLiteDatabase?) {
@@ -53,16 +52,20 @@ class AppDbHelper(ctx: Context) : SQLiteOpenHelper(ctx, DATABASE_NAME, null, DAT
             Log.d("IDROW", idRow.toString())
         }
     }
-    fun saveFollowerOnDb(post : List<FollowerDB>) {
+
+    fun saveFollowerOnDb(post: Array<Payload>) {
         val db = this.writableDatabase
         post.forEach {
             val insert = ContentValues().apply {
                 put(FollowerContract.FollowerEntry.COLUMN_NAME, it.name)
                 put(FollowerContract.FollowerEntry.COLUMN_ID, it.id)
+                put(FollowerContract.FollowerEntry.COLUMN_DESCRIPTION, it.description)
             }
-            val idRow = db?.insert(FollowerContract.FollowerEntry.TABLE_NAME, null, insert)
+            val idRows = db?.insert(FollowerContract.FollowerEntry.TABLE_NAME, null, insert)
+            Log.d("IDROWS", idRows.toString())
         }
     }
+
     fun getPostsFromDb(): List<Post> {
         val lista = arrayListOf<Post>()
         val db = this.readableDatabase
@@ -71,20 +74,42 @@ class AppDbHelper(ctx: Context) : SQLiteOpenHelper(ctx, DATABASE_NAME, null, DAT
         while (cursor!!.moveToNext()) {
             lista.add(
                 Post(
-                    cursor.getInt(cursor.getColumnIndexOrThrow(PostContract.PostEntry.COLUMN_PROFILE_ID)).toString(),
-                    cursor.getInt(cursor.getColumnIndexOrThrow(PostContract.PostEntry.COLUMN_ID)).toString(),
+                    cursor.getInt(cursor.getColumnIndexOrThrow(PostContract.PostEntry.COLUMN_PROFILE_ID))
+                        .toString(),
+                    cursor.getInt(cursor.getColumnIndexOrThrow(PostContract.PostEntry.COLUMN_ID))
+                        .toString(),
                     cursor.getString(cursor.getColumnIndexOrThrow(PostContract.PostEntry.COLUMN_TITLE)),
                     "",
                     cursor.getString(cursor.getColumnIndexOrThrow(PostContract.PostEntry.COLUMN_UPLOADTIME)),
                     Profile(
-                        cursor.getInt(cursor.getColumnIndexOrThrow(PostContract.PostEntry.COLUMN_PROFILE_ID)).toString(),
+                        cursor.getInt(cursor.getColumnIndexOrThrow(PostContract.PostEntry.COLUMN_PROFILE_ID))
+                            .toString(),
                         "", "", "", "", ""
                     )
-
                 )
             )
         }
+        cursor.close()
         return lista;
+    }
+    fun getFollowerFromDB() : Array<Payload>{
+        val follower = arrayListOf<Payload>()
+        val db = this.readableDatabase
+        val cursor =
+            db?.query(FollowerContract.FollowerEntry.TABLE_NAME, null, null, null, null, null, null)
+        while (cursor!!.moveToNext()) {
+            follower.add(
+                Payload(
+                    cursor.getInt(cursor.getColumnIndexOrThrow(FollowerContract.FollowerEntry.COLUMN_ID)).toString(),
+                    cursor.getString(cursor.getColumnIndexOrThrow(FollowerContract.FollowerEntry.COLUMN_NAME)),
+                    cursor.getString(cursor.getColumnIndexOrThrow(FollowerContract.FollowerEntry.COLUMN_DESCRIPTION)),
+                   ""
+                )
+            )
+
+        }
+        cursor.close()
+        return follower.toTypedArray()
     }
 
     companion object {
--- app/src/main/java/com/example/instagrammo/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/instagrammo/ui/fragment/HomeFragment.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -44,32 +45,38 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        RetrofitController.getClient.getFollowerPost().enqueue(object : Callback<HomeWrapperPostBean>{
-            override fun onFailure(call: Call<HomeWrapperPostBean>, t: Throwable) {
-                val linearLayoutManager = LinearLayoutManager(context)
-                linearLayoutManager.stackFromEnd = true
-                linearLayoutManager.reverseLayout = true
-                HomeFollowerPosts.layoutManager = linearLayoutManager
-                HomeFollowerPosts.addItemDecoration(DividerItemDecoration(context, LinearLayoutManager.VERTICAL))
-                val adapterFollowerPost =
-                    HomeFollowerPostAdapter(db.getPostsFromDb())
-                HomeFollowerPosts.adapter = adapterFollowerPost
+        RetrofitController.getClient.getFollowerPost()
+            .enqueue(object : Callback<HomeWrapperPostBean> {
+                override fun onFailure(call: Call<HomeWrapperPostBean>, t: Throwable) {
+                    val linearLayoutManager = LinearLayoutManager(context)
+                    linearLayoutManager.stackFromEnd = true
+                    linearLayoutManager.reverseLayout = true
+                    HomeFollowerPosts.layoutManager = linearLayoutManager
+                    HomeFollowerPosts.addItemDecoration(
+                        DividerItemDecoration(
+                            context,
+                            LinearLayoutManager.VERTICAL
+                        )
+                    )
+                    val adapterFollowerPost =
+                        HomeFollowerPostAdapter(db.getPostsFromDb())
+                    HomeFollowerPosts.adapter = adapterFollowerPost
 
-            }
-            override fun onResponse(
-                call: Call<HomeWrapperPostBean>,
-                response: Response<HomeWrapperPostBean>
-            ) {
-                createPost(response)
-                val daSalvare = arrayListOf<PostDb>()
-                response!!.body()!!.payload.forEach{
-                    val nuovo :PostDb = PostDb(it.profileId,it.postId,it.title,it.uploadTime)
-                    daSalvare.add(nuovo)
                 }
-                db.savePostOnDb(daSalvare)
-                // AppDbHelper(context!!).saveFollowerOnDb(followers)
-            }
-        })
+
+                override fun onResponse(
+                    call: Call<HomeWrapperPostBean>,
+                    response: Response<HomeWrapperPostBean>
+                ) {
+                    createPost(response)
+                    val daSalvare = arrayListOf<PostDb>()
+                    response!!.body()!!.payload.forEach {
+                        val nuovo: PostDb = PostDb(it.profileId, it.postId, it.title, it.uploadTime)
+                        daSalvare.add(nuovo)
+                    }
+                    db.savePostOnDb(daSalvare)
+                }
+            })
         return inflater.inflate(R.layout.home_fragment_layout, container, false)
 
     }
@@ -78,44 +85,46 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         view.progressBar2.visibility = View.VISIBLE
-        val lManager = LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL,false )
+        val lManager = LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL, false)
         rView.layoutManager = lManager
         val retrofit = RetrofitController.getClient
-        retrofit.getStoriesList(Session.profileId.toString()).enqueue(object :Callback<StoriesResponse>{
-            override fun onFailure(call: Call<StoriesResponse>, t: Throwable) {
+        retrofit.getStoriesList(Session.profileId.toString())
+            .enqueue(object : Callback<StoriesResponse> {
+                override fun onFailure(call: Call<StoriesResponse>, t: Throwable) {
+                    view.progressBar2.visibility = View.INVISIBLE
+                    val lManager =
+                        LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL, false)
+                    rView.layoutManager = lManager
+                    rView.adapter = Adapter(db.getFollowerFromDB())
+                }
 
-            }
-
-            override fun onResponse(
-                call: Call<StoriesResponse>,
-                response: Response<StoriesResponse>
-            ) {
-                view.progressBar2.visibility = View.GONE
-
-
-               // val ad = Adapter( response!!.body()!!.payload)
-                Log.d("response",response!!.body()!!.payload.toString())
-                view.rView.adapter =
-                    Adapter(response!!.body()!!.payload)
-
-            }
-        })
-
+                override fun onResponse(
+                    call: Call<StoriesResponse>,
+                    response: Response<StoriesResponse>
+                ) {
+                    view.progressBar2.visibility = View.GONE
+                    db.saveFollowerOnDb(response.body()!!.payload)
+                    // val ad = Adapter( response!!.body()!!.payload)
+                    Log.d("response", response!!.body()!!.payload.toString())
+                    view.rView.adapter = Adapter(response!!.body()!!.payload)
+                }
+            })
     }
 
-    private fun createPost(response : Response<HomeWrapperPostBean>) : RecyclerView {
+    private fun createPost(response: Response<HomeWrapperPostBean>): RecyclerView {
         val linearLayoutManager = LinearLayoutManager(this.context)
         linearLayoutManager.stackFromEnd = true
         linearLayoutManager.reverseLayout = true
         HomeFollowerPosts.layoutManager = linearLayoutManager
-        HomeFollowerPosts.addItemDecoration(DividerItemDecoration(context, LinearLayoutManager.VERTICAL))
+        HomeFollowerPosts.addItemDecoration(
+            DividerItemDecoration(
+                context,
+                LinearLayoutManager.VERTICAL
+            )
+        )
         val adapterFollowerPost =
             HomeFollowerPostAdapter(response.body()!!.payload)
         HomeFollowerPosts.adapter = adapterFollowerPost
         return HomeFollowerPosts
     }
-
-
-
-
 }
