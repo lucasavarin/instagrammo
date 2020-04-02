9a3014772f8cef5eb43a53bac7c0741f603b9dec
Scott Thushyanthan <scott.thushyanthan@lynxspa.com>
2020-04-02 12:38:20 +0200

    Detail activity: Header and back button

--- app/src/main/java/com/lynxspa/instagrammo/DetailActivity.kt
+++ app/src/main/java/com/lynxspa/instagrammo/DetailActivity.kt
@@ -1,12 +1,22 @@
 package com.lynxspa.instagrammo
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_detail.*
 
 class DetailActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+        getBackToMainActivity()
+    }
+
+    fun getBackToMainActivity(){
+        backButton.setOnClickListener {
+            val intent = Intent(applicationContext, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 
 }
