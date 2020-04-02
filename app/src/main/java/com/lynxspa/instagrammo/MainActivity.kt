645f8f6ae340238e2d621aec429d89d63039f17b
Alessandro <alejolly98@gmail.com>
2020-04-02 12:17:17 +0200

    commit 1

--- app/src/main/java/com/lynxspa/instagrammo/MainActivity.kt
+++ app/src/main/java/com/lynxspa/instagrammo/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.lynxspa.instagrammo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +11,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        passActivity()
     }
+
+    private fun passActivity() {
+        Button.setOnClickListener(this::passActivity2)
+    }
+    private fun passActivity2(view : View) : Unit {
+        val intent = Intent(applicationContext, DetailActivity::class.java)
+        startActivity(intent)
+
+    }
+
 }
